0ef56d5d0090883f7237c8bbe043fff40fb2bcaa
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-22 20:15:23 +0530

    Create preview.js

---
+++ js/preview.js
@@ -0,0 +1,24 @@
+const fileInput = document.querySelector('input[type="file"]');
+
+const previewContainer = document.createElement("div");
+previewContainer.style.marginTop = "20px";
+document.querySelector("form").appendChild(previewContainer);
+
+fileInput.addEventListener("change", function () {
+    previewContainer.innerHTML = "";
+
+    Array.from(this.files).forEach(file => {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            const img = document.createElement("img");
+            img.src = e.target.result;
+            img.style.width = "120px";
+            img.style.margin = "10px";
+            img.style.borderRadius = "8px";
+            previewContainer.appendChild(img);
+        }
+
+        reader.readAsDataURL(file);
+    });
+});
