77c1660f62aca0e37ed5db7e3fe45aa7ac618301
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-23 00:50:48 +0530

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,24 +1,19 @@
 const lengthInput = document.getElementById("length");
 const widthInput = document.getElementById("width");
 const totalArea = document.getElementById("totalArea");
-
 function calculateArea() {
     const length = parseFloat(lengthInput.value) || 0;
     const width = parseFloat(widthInput.value) || 0;
     totalArea.value = length * width + " sq ft";
 }
-
 lengthInput.addEventListener("input", calculateArea);
 widthInput.addEventListener("input", calculateArea);
-
 
 // Video Preview
 const videoInput = document.getElementById("videoUpload");
 const videoPreview = document.getElementById("videoPreview");
-
 videoInput.addEventListener("change", function () {
     videoPreview.innerHTML = "";
-
     const file = this.files[0];
     if (file) {
         const video = document.createElement("video");
