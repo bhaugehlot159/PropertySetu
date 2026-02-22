f3447d1bed3af9b13785d871d4afff36261121f8
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-23 00:52:16 +0530

    Update filter.js

--- js/js/filter.js
+++ js/js/filter.js
@@ -1,16 +1,10 @@
 const filterSelect = document.getElementById("filterSelect");
 const propertyCards = document.querySelectorAll(".property-card");
-
 filterSelect.addEventListener("change", function() {
     const value = this.value;
-
     propertyCards.forEach(card => {
-        if(value === "all") {
-            card.style.display = "inline-block";
-        } else if(card.querySelector(".tag").classList.contains(value)) {
-            card.style.display = "inline-block";
-        } else {
-            card.style.display = "none";
-        }
+        if(value === "all") card.style.display = "inline-block";
+        else if(card.querySelector(".tag").classList.contains(value)) card.style.display = "inline-block";
+        else card.style.display = "none";
     });
 });
