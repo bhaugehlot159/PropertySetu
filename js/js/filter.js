0dca22c5a986298292af6cb138c7156223c52276
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-23 00:22:33 +0530

    Create js/filter.js

---
+++ js/js/filter.js
@@ -0,0 +1,16 @@
+const filterSelect = document.getElementById("filterSelect");
+const propertyCards = document.querySelectorAll(".property-card");
+
+filterSelect.addEventListener("change", function() {
+    const value = this.value;
+
+    propertyCards.forEach(card => {
+        if(value === "all") {
+            card.style.display = "inline-block";
+        } else if(card.querySelector(".tag").classList.contains(value)) {
+            card.style.display = "inline-block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+});
