f6f37995e94eb7e8db5f1ec268f61b50b1e995db
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-26 00:17:50 +0530

    Create server/models/Review.js

---
+++ server/models/Review.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const reviewSchema = new mongoose.Schema({
+    property: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Property",
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String
+    }
+}, { timestamps: true });
+
+export default mongoose.model("Review", reviewSchema);
