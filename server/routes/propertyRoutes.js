52110d25ba5d16a087e98f11268d7b204eadc4c1
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-26 00:05:20 +0530

    Update propertyRoutes.js

--- server/routes/propertyRoutes.js
+++ server/routes/propertyRoutes.js
@@ -8,3 +8,22 @@ router.post("/", protect, addProperty);
 router.get("/", getProperties);
 
 export default router;
+
+import express from "express";
+import { 
+    addProperty, 
+    getProperties, 
+    updateProperty, 
+    deleteProperty 
+} from "../controllers/propertyController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", protect, addProperty);
+router.get("/", getProperties);
+
+router.put("/:id", protect, updateProperty);
+router.delete("/:id", protect, deleteProperty);
+
+export default router;
