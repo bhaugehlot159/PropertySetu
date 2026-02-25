cf9778d53553b522c250990659fdbdec9b6cd34b
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-25 23:58:36 +0530

    Create server/models/Property.js

---
+++ server/models/Property.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const propertySchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    location: { type: String, required: true },
+    price: { type: Number, required: true },
+    type: { type: String, enum: ["buy", "rent"], required: true },
+    description: { type: String },
+    image: { type: String },
+    featured: { type: Boolean, default: false },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, { timestamps: true });
+
+export default mongoose.model("Property", propertySchema);
