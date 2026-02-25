fb967697b3114ef3aaf60ee17c3d151dd85737b1
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-25 23:51:11 +0530

    Create server/models/User.js

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["buyer", "seller", "admin"],
+        default: "buyer"
+    },
+    verified: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
