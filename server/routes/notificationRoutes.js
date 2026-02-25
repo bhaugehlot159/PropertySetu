283c7a51927d1a5eb37ded2bc08abe72053b622e
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-26 00:36:06 +0530

    Create server/routes/notificationRoutes.js

---
+++ server/routes/notificationRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { getMyNotifications, markAsRead } from "../controllers/notificationController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", protect, getMyNotifications);
+router.put("/read/:id", protect, markAsRead);
+
+export default router;
