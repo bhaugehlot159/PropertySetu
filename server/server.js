35ec2e05594fd87b1dfe1739ac8f5c969db81314
bhaugehlot159 <bhaugehlot159@gmail.com>
2026-02-25 23:48:05 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// MongoDB Connect
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+// Test Route
+app.get("/", (req, res) => {
+    res.json({ message: "PropertySetu API Running" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
